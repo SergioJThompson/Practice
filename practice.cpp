f20fcdcad62d199328cad0ec01744d0639b47833
Sergio Thompson <72581976+SergioJThompson@users.noreply.github.com>
2023-01-08 18:14:16 -0700

    Add files via upload

---
+++ practice.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Welcome!" << endl;
+    cout << endl;
+    cout <<"I said, WELCOME!" << endl;
+}
---
+++ print-sum-two-numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x = 5;
+    int y = 10;
+
+    cout << "Print the sum of two numbers :" << endl;
+    cout << "-----------------------------------" << endl;
+    cout << "The sum of " << x << " and " << y << " is " << x + y << endl;
+}
