e7e539a4d35253ead574cc47891aec1802ec57dc
Sergio Thompson <72581976+SergioJThompson@users.noreply.github.com>
2023-02-21 14:00:06 -0700

    Add files via upload

--- Tic-Tac-Toe/main.cpp
+++ Tic-Tac-Toe/main.cpp
@@ -16,6 +16,14 @@ void loop(SDL_Window *window, SDL_Surface *image_surface, Object square_arr[], i
                     break;
             }
         }
+        // toggle image state of each object
+        for (int i = 0; i < arr_size; i++) {
+            square_arr[i].toggleImage();
+        }
+
+        // clear the surface
+        SDL_FillRect(image_surface, nullptr, SDL_MapRGB(image_surface->format, 255, 255, 255));
+
         SDL_BlitSurface(image_surface, nullptr, SDL_GetWindowSurface(window), nullptr);
         for (int i = 0; i < arr_size; i++) {
             square_arr[i].draw(SDL_GetWindowSurface(window));
@@ -57,17 +65,27 @@ int main() {
     }
 
 
-    SDL_Surface* sprite_surface = IMG_Load("/Users/sergiojthompson/Programs/C++/Tic-Tac-Toe/images/x.jpg");
+    SDL_Surface* sprite_surface = IMG_Load("/Users/sergiojthompson/Programs/C++/Tic-Tac-Toe/images/x.png");
 
-    Object squareA1(sprite_surface, 0, 159);
-    Object squareB1(sprite_surface, 0, 300);
-    Object squareC1(sprite_surface, 0, 450);
+    Object squareA1(sprite_surface, 2, 168);
+    Object squareA2(sprite_surface, 2, 328);
+    Object squareA3(sprite_surface, 2, 490);
 
-    Object square_arr[3] = {
-        squareA1, squareB1, squareC1
+    Object squareB1(sprite_surface, 168, 161);
+    Object squareB2(sprite_surface, 168, 328);
+    Object squareB3(sprite_surface, 168, 490);
+
+    Object squareC1(sprite_surface, 335, 161);
+    Object squareC2(sprite_surface, 335, 328);
+    Object squareC3(sprite_surface, 335, 490);
+
+    Object square_arr[9] = {
+        squareA1, squareA2, squareA3,
+        squareB1, squareB2, squareB3,
+        squareC1, squareC2, squareC3
     };
 
-    loop(window, image_surface, square_arr, 3);
+    loop(window, image_surface, square_arr, 9);
 
     SDL_FreeSurface(image_surface);
     SDL_DestroyWindow(window);
--- Tic-Tac-Toe/object.cpp
+++ Tic-Tac-Toe/object.cpp
@@ -1,16 +1,56 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#include <string>
+
+enum ImageState : int { Empty, X, O };
 
 class Object {
-    public:
-        Object(SDL_Surface *surface, int x, int y) : m_surface(surface), m_x(x), m_y(y) {}
+public:
+    Object(SDL_Surface *surface, int x, int y) : m_surface(surface), m_x(x), m_y(y), m_imageState(Empty) {}
 
-        void draw(SDL_Surface *surface) {
+
+    void draw(SDL_Surface *surface) {
+        if (m_imageState != Empty) {
             SDL_Rect rect = {m_x, m_y, m_surface->w, m_surface->h};
             SDL_BlitSurface(m_surface, nullptr, surface, &rect);
         }
+    }
 
-    private:
-        SDL_Surface *m_surface;
-        int m_x, m_y;
+    void toggleImage() {
+        switch (m_imageState) {
+            case X:
+                m_surface = IMG_Load("/Users/sergiojthompson/Programs/C++/Tic-Tac-Toe/images/o.png");
+                m_imageState = O;
+                break;
+            case 2:
+                m_imageState = O;
+                break;
+            case 0:
+                m_imageState = Empty;
+            default:
+                break;
+        }
+    }
+
+    void toggleImage(ImageState s) {
+        switch (s) {
+            case X:
+                m_surface = IMG_Load("/Users/sergiojthompson/Programs/C++/Tic-Tac-Toe/images/x.png");
+                m_imageState = X;
+                break;
+            case O:
+                m_surface = IMG_Load("/Users/sergiojthompson/Programs/C++/Tic-Tac-Toe/images/o.png");
+                m_imageState = O;
+                break;
+            case Empty:
+                m_imageState = Empty;
+            default:
+                break;
+        }
+    }
+
+private:
+    SDL_Surface *m_surface;
+    int m_x, m_y;
+    ImageState m_imageState;
 };
