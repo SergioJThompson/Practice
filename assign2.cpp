f40d6de7533b3167a21b14c89ccfc9f0dcd2911f
Sergio Thompson <72581976+SergioJThompson@users.noreply.github.com>
2023-03-24 20:33:13 -0600

    Add files via upload

--- assign2.cpp
+++ assign2.cpp
@@ -5,39 +5,21 @@
 
 #include <iostream>
 
-using namespace std;
-
-double get_double_from_user(string msg) {
-    cout << msg;
-    double d;
-    cin >> d;
-    return d;
-}
-
-
-string decision(double gpa, double sat) {
-    if (gpa >= 3.5) {
-        if (sat >  1400) return "Admit";
-        if (sat >= 1100) return "Waitlist";
-    }
-
-    if (gpa >= 3.0) {
-        if (sat >  2000) return "Admit";
-        if (sat >= 1500) return "Waitlist";
-    }
-
-    if (sat >= 2200) return "Admit";
-
-    return "Deny";
-}
 
 int main()
 {
-    double gpa = get_double_from_user("Enter GPA: ");
-    if (gpa >= 4.0)
-        cout << "\nAdmit\n";
-    else {
-        double sat = get_double_from_user("Enter SAT: ");
-        cout << "\n" + decision(gpa, sat) + "\n";
-    }
+    int a;
+    int b;
+
+    std::cout << "Enter a: ";
+    std::cin >> a;
+
+    std::cout << "Enter b: ";
+    std::cin >> b;
+
+    int* ptrA = &a;
+    int* ptrB = &b;
+
+    std::cout << "Pointer A: " << ptrA;
+    std::cout << "Pointer B: " << ptrB;
 }
