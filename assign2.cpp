fb09a6b63985b1bb4736cbdba3fc9474a258291f
Sergio Thompson <72581976+SergioJThompson@users.noreply.github.com>
2023-03-26 12:34:02 -0600

    Add files via upload

--- assign2.cpp
+++ assign2.cpp
@@ -5,13 +5,20 @@
 
 #include <iostream>
 
+int print_fib(int prev_fib_num, int current_fib_num, int nums_remaining) {
+    if (nums_remaining <= 0)
+        return 0;
 
-int main()
-{
-    int val = 5;
-    std::cout << val;
+    std::cout << current_fib_num << " ";
+    nums_remaining--;
 
-    int* ptr = &val;
-    *ptr += 5;
-    std::cout << val;
+    if (current_fib_num == 0)
+        return print_fib(0, 1, nums_remaining);
+    return print_fib(current_fib_num, prev_fib_num + current_fib_num, nums_remaining);
+}
+
+int main() {
+    int num_of_nums;
+    std::cin >> num_of_nums;
+    print_fib(0, 0, num_of_nums);
 }
