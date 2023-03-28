87db1df31e4a55ea41117f9f094fbe273beed624
Sergio Thompson <72581976+SergioJThompson@users.noreply.github.com>
2023-03-28 11:55:48 -0600

    Add files via upload

--- assign2.cpp
+++ assign2.cpp
@@ -4,28 +4,30 @@
 */
 
 #include <iostream>
+#include <string>
 #include <cmath>
 
-int calc_gcd(int m, int n) {
-    int lowest = std::min(m, n);
-    int gcd = 1;
-    for (int div = lowest; div >= 2; div--) {
-        if (m % div == 0 && n % div == 0) {
-            gcd = div;
-            break;
-        }
+bool is_palindrome(std::string s) {
+    std::string lowered;
+    for (int i = 0; i < s.size(); i++) {
+        if (isalpha(s[i]))
+            lowered += tolower(s[i]);
     }
-    return gcd;
+
+    std::string lowered_reversed;
+    for (int i = 0; i < lowered.size(); i++) {
+        lowered_reversed = lowered[i] + lowered_reversed;
+    }
+
+    return (lowered == lowered_reversed);
 }
 
 int main() {
-    int m;
-    int n;
-    int gcd;
+    std::string s;
+    getline (std::cin, s);
 
-    std::cin >> m;
-    std::cin >> n;
-
-    gcd = calc_gcd(m, n);
-    std::cout << gcd;
+    if (is_palindrome(s))
+        std::cout << "It's a palindrome!";
+    else
+        std::cout << "Not a palindrome.";
 }
